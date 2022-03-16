6a4df67758d0834a443666056a6ba37c7c3ae200
PANWAZII <panwa1379@gmail.com>
2022-03-16 13:38:40 +0700

    add getAllDocuments public api

--- server/routes/Public/publicApi.js
+++ server/routes/Public/publicApi.js
@@ -5,7 +5,24 @@ const router = express.Router();
 router.get("/getAllDocNum", async (req, res) => {
   try {
     const allDocuments = await lessthanfivems.find();
-    res.status(200).json({count:allDocuments.length});
+    res.status(200).json({ count: allDocuments.length });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/getAllDocuments", async (req, res) => {
+  try {
+    const allDocuments = await lessthanfivems.find();
+    const Data = allDocuments.map((item) => {
+      const container = {};
+      container["name"] = item.project_name;
+      container["department"] = item.department_name;
+      container["date"] = item.created_date;
+      container["status"] = item.approval_status;
+      return container;
+    });
+    res.status(200).json(Data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
