2a864ed6099fa1a185cedd00b0b5300119f3a54c
PANWAZII <panwa1379@gmail.com>
2022-03-30 14:33:33 +0700

    fix route

--- dms-system/store/api.js
+++ dms-system/store/api.js
@@ -98,43 +98,43 @@ export const actions = {
   async lessThanCreatDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .post('/lessthanfives/createNewDocument', { data }, { headers })
+      .post('/documents/createNewDocument', { data }, { headers })
       .then((res) => res.data)
   },
   async getAllDocByUid({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getMyDoc', data, { headers })
+      .get('/documents/getMyDoc', data, { headers })
       .then((res) => res.data)
   },
   async getApprovedDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getApproved', data, { headers })
+      .get('/documents/getApproved', data, { headers })
       .then((res) => res.data)
   },
   async getRejectedDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getRejected', data, { headers })
+      .get('/documents/getRejected', data, { headers })
       .then((res) => res.data)
   },
   async getWaitingDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getWaiting', data, { headers })
+      .get('/documents/getWaiting', data, { headers })
       .then((res) => res.data)
   },
   async getDmsDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getDms', data, { headers })
+      .get('/documents/getDms', data, { headers })
       .then((res) => res.data)
   },
   async getMophDoc({ getters }, data) {
     const headers = getAuthToken(this.$cookies)
     return await axios
-      .get('/lessthanfives/getMoph', data, { headers })
+      .get('/documents/getMoph', data, { headers })
       .then((res) => res.data)
   },
   async getAllSources({ getters }, data) {
@@ -144,7 +144,7 @@ export const actions = {
   },
   async getBudgetYear({ getters }, data) {
     return await axios
-      .get('/lessthanfives/getBudgetYear')
+      .get('/documents/getBudgetYear')
       .then((res) => res.data)
   },
 }
