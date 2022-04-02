27ff5eed2425ece7faea0fa8f60f23183d214e37
PANWAZII <panwa1379@gmail.com>
2022-04-03 03:23:31 +0700

    add config file

---
+++ server/firebaseStorageConfig.js
@@ -0,0 +1,17 @@
+const config = {
+  type: "service_account",
+  project_id: "dms-document-tracking-ef733",
+  private_key_id: "9c9abf552e00813f73ad9180fe1a8156c9821eb6",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCn9/8uewrOFxAh\nVknVx9T4bLwiqwwj+vKg68PoR1B1ud+8VGJPBCIp+sxKUF8ZjGy5kOQSnSqSFJ7P\nTNvogii0yNH4ptNuOzUPaTP7o53ddxX9FOhPeOABVnV2j9+tBkd3pmwnDBXed6wT\nnV7ZQj1rWudbaA8eWcyCIK+y0BAkW5cG37cHj+26zoptZGWXpC1jb9T0hAu85p0u\n+Sw3QdwGzYf59YUeAQdjoaDQ4xLJzLGq7OQhbx74ljWmGx+S3soe9QQTOiW9fWjh\nVVYfIucn7rGLrtH6CRAjD4qQ4Z1u4Am60068QZSHTj5rwU0wgEqHvjl7CzBN3iuE\nTah+se5jAgMBAAECggEACsf1tpeKMYE0dVdTBOg/KLTd/IGrFVIhqaeIWwvcw7mq\n/S0TZEv5H2WsICb5e6vSPLTqlrFEzryJFvgxc0KLd1Meew3GK2CcfsE7Z27wjor6\nu9RQNu2kvyE8B+CjNDlfc/QYJAd4Dd/ziQzETkCcrZvXlrveoI2V/VI4xMrrBlJK\nBpgPwd6Y9aZwPb9qqPM3AHX0te1iV1R11LeJQoy2Qi8sMxSiaqZFUNdmd2VrqltG\ngigjFrq1R3Z22H1+osHhTnO3H37hG8FRBc9/7lWDxAXx58tVAjBzk75L5fFgsKXF\nI6GfyA7efqjro8L+mrsHsvR5ufT2w2O2P2ZM16WpaQKBgQDT2b9KvnfTFl+/VBRB\ne1KrU5WOY87ZWxLIDpT0nFiNm2FAwSa8L60lHYwApCOS/7pZSGyJuoPDVVxR3Dce\n5UQq+NcqbKKJ8tih1oDT5OYVs1PsJSxflf9ypmdrlZJ9ypB+VO8asTWo9RHS8Q15\nyQUFT0sD6GWoX9oh9t9Zd+GFaQKBgQDK+SYAJQtlJeowoSORa4hvfU8Fi57QqUl9\n94C7DbjYLa+/IKg7PF3tdwBWRmBQLm4wQL8k0xOlQ/+pAJyjhPMi02izgp/4YQ8r\n0NS0/kupWP4YlDrjvOSY4VqzZnasX/gqRjqoXMzl+HPX6uLoYvhHBEiwU/N6F+sL\n8xdzBwbf6wKBgH1L1ti1KBd/rydw6f4Em1NbPyQVIdzqAYSzIyQ9aB5wnKd41ZkN\ntaEzBX/HlIa3mxZ9QDyBRLb9wHll/m/Wd2fW/yNHVyaxq7CkNlOcmPySsHsFTWZH\nR+seQiKeNyMQ8D7XqwclcjA4qc8R5L93xfFrAVoRVDauYkweP+Ehhw+pAoGBAKy5\nXKce3xWSMDp1xejIE3CcuBRF/j4e8VVqu3dyc+wy5K3GwqK1+lBLxlOky+0vtcEG\nOCLEJKGsvfE5fhKA3s6yDRVFgZBCOEDXE0t6YzSXcJPGAF8wrW+TxjWIalDB0lNX\nNC912T+SMDJ7xeBv6Eu2z4yNxFIP7ItcsIXIYwdFAoGABNkcMvIluB2r5ojJAURR\nYlpCbvdB5OFe50LIa+QXvmceInHmnxemhOnlDE3IFj4Qgp7AA0qWPF2srAsVgeZg\nCgD+8bjDerq1fPscUuV4CbWWodosiBWVHm4kHsXC7jar23ACVeIh5eFGgD9zwR+8\nEoTvkjgq+QdzPVuBtU2CBbw=\n-----END PRIVATE KEY-----\n",
+  client_email:
+    "firebase-adminsdk-t9amp@dms-document-tracking-ef733.iam.gserviceaccount.com",
+  client_id: "111908238102259724917",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t9amp%40dms-document-tracking-ef733.iam.gserviceaccount.com",
+};
+
+export default config;
