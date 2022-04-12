ac3b0d99c0fda295f5264c3278ab1d38a1763d43
PANWAZII <panwa1379@gmail.com>
2022-04-12 15:05:03 +0700

    add universal firebase conf

---
+++ server/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyAA4gEFyqUFR-XH0LYGbysywjdIBB_Exn4",
+  authDomain: "dms-document-tracking-ef733.firebaseapp.com",
+  projectId: "dms-document-tracking-ef733",
+  storageBucket: "dms-document-tracking-ef733.appspot.com",
+  messagingSenderId: "331164339207",
+  appId: "1:331164339207:web:72498673e3b1db30ef6ea2",
+  measurementId: "G-DRWDF7NT3R",
+};
+
+export default config;
