03f3eed13dc90c7052b0413723bb20d4a194d968
Panwa Muangsong <panwa1379@gmail.com>
2022-04-19 15:59:09 +0700

    add project_num & remove unused field

--- server/models/AllDocument/document.js
+++ server/models/AllDocument/document.js
@@ -5,6 +5,10 @@ const documentSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
+  project_num: {
+    type: String,
+    required: false,
+  },
   project_name: {
     type: String,
     required: false,
@@ -25,68 +29,8 @@ const documentSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
-  quantity: {
-    type: Number,
-    required: false,
-  },
-  unit: {
-    type: String,
-    required: false,
-  },
-  price_unit: {
-    type: Number,
-    required: false,
-  },
   sum: {
     type: Number,
-    required: false,
-  },
-  method: {
-    type: String,
-    required: false,
-  },
-  destination: {
-    type: String,
-    required: false,
-  },
-  cert: {
-    type: String,
-    required: false,
-  },
-  list_old: {
-    type: String,
-    required: false,
-  },
-  locate_old: {
-    type: String,
-    required: false,
-  },
-  year_old: {
-    type: String,
-    required: false,
-  },
-  obstacle: {
-    type: String,
-    required: false,
-  },
-  purpose_of_use: {
-    type: String,
-    required: false,
-  },
-  compare: {
-    type: String,
-    required: false,
-  },
-  major: {
-    type: String,
-    required: false,
-  },
-  quantity_major: {
-    type: Number,
-    required: false,
-  },
-  specific_info: {
-    type: String,
     required: false,
   },
   created_date: {
