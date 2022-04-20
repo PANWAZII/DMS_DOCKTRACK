5438ecaebbd35b7aafc3e7c730c2e3df19f0f854
Panwa Muangsong <panwa1379@gmail.com>
2022-04-20 20:23:53 +0700

    add user_uid checker

--- dms-system/middleware/middleware-admin-auth.js
+++ dms-system/middleware/middleware-admin-auth.js
@@ -1,9 +1,11 @@
 export default async function ({ store, redirect }) {
   const user_uid = store.getters.uid
-  const auth = await store.dispatch('api/getLevel', { uid: user_uid })
-  console.log('this is uid admin mid ', user_uid)
-  console.log('this is level admin mid ', auth.level)
-  if (!(store.getters.isAuthenticated && auth.level === 'admin')) {
+  if (!user_uid) {
     return redirect('/login')
   }
+  const auth = await store.dispatch('api/getLevel', { uid: user_uid })
+  if (!(store.getters.isAuthenticated && auth.level === 'admin')) {
+    return redirect('/adminLogin')
+  }
+  // else if(){}
 }
--- dms-system/middleware/middleware-user-auth.js
+++ dms-system/middleware/middleware-user-auth.js
@@ -1,8 +1,9 @@
 export default async function ({ store, redirect }) {
   const user_uid = store.getters.uid
+  if (!user_uid) {
+    return redirect('/login')
+  }
   const auth = await store.dispatch('api/getLevel', { uid: user_uid })
-  console.log("this is uid user mid ",user_uid);
-  console.log("this is level user mid ",auth.level);
   if (!(store.getters.isAuthenticated && auth.level === 'user')) {
     return redirect('/login')
   }
